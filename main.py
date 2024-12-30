63cf99e814e363841d38bc048f00c53c9cc3a8c5
SAC <sachin@craftech360.com>
2024-12-30 11:50:00 +0530

    updated-aws-cred

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ import json
 import urllib.request
 import random
 import boto3
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
+# from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
 from contextlib import asynccontextmanager
 from threading import Thread
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+
 
 # Initialize SQLite database
 def init_db():
